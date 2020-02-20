efafdbed7c351fdada197d3b28236e8572ecd5ef
marknesh <markmunene.nesh@gmail.com>
2020-02-20 21:31:19 +0300

    add operating in readme.md

--- app/config.py
+++ app/config.py
@@ -13,7 +13,6 @@ class Config:
 
 
 class ProdConfig(Config):
-
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
--- app/main/error.py
+++ app/main/error.py
@@ -1,10 +1,9 @@
 from flask import url_for, redirect,render_template
 from . import main
 from flask import flash
-from app.auth.forms import LoginForm
 
 
 @main.app_errorhandler(401)
 def error(error):
-    loginform=LoginForm()
-    return render_template('auth/login.html',loginform=loginform)
+
+    return redirect(url_for('auth.login'),401)
