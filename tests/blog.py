3ae2bb8f8e035fa3d2bd52f01c31bddd621720a1
marknesh <markmunene.nesh@gmail.com>
2020-02-21 08:20:08 +0300

    test user class and password setter

--- tests/blog.py
+++ tests/blog.py
@@ -1,9 +1,11 @@
-from app.models import Qoutes
+from app.models import Qoutes,User,Comments
 import unittest
 
 class quote_test(unittest.TestCase):
     def setUp(self):
         self.new_quote=Qoutes(id=1,quote="poa",author="at all")
+
+
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_quote, Qoutes))
